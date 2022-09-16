b04dc52e975f1b799156bab712da3af7e81c63ce
Alexandre Godard <alexandre@godard.me>
2022-09-16 17:48:54 -0400

    Disable a couple of react rules

--- index.js
+++ index.js
@@ -4,4 +4,18 @@ module.exports = {
         "react-hooks",
         "react",
     ],
+    "rules": {
+        "react/prop-types": 0,
+        "react/require-default-props": 0,
+        "react/no-danger": 0,
+        "react/jsx-props-no-spreading": 0,
+        "react/function-component-definition": 0,
+        "react/jsx-filename-extension": [1,
+            {
+                "extensions": [
+                    ".tsx", ".jsx"
+                ]
+            }
+        ]
+    }
 };
